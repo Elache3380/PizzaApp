c4ff5b0df7afc491800845a243ad62afbfc1983c
Lionel Hardy <l.hardy@afelio.be>
2020-09-04 13:42:42 +0200

    :sparkles: Initial migration

---
+++ PizzaApp.Data/Data/PizzaAppContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaApp.Data.Data
+{
+    public class PizzaAppContextFactory : IDesignTimeDbContextFactory<PizzaAppContext>
+    {
+        public PizzaAppContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<PizzaAppContext>();
+            optionsBuilder.UseSqlite("Data Source=blog.db");
+
+            return new PizzaAppContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ PizzaApp.Data/Migrations/20200904114223_InitialCreate.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PizzaApp.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pizza",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(maxLength: 250, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pizza", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Topping",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(maxLength: 250, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Topping", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PizzaTopping",
+                columns: table => new
+                {
+                    PizzaId = table.Column<int>(nullable: false),
+                    ToppingId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PizzaTopping", x => new { x.PizzaId, x.ToppingId });
+                    table.ForeignKey(
+                        name: "FK_PizzaTopping_Pizza_PizzaId",
+                        column: x => x.PizzaId,
+                        principalTable: "Pizza",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PizzaTopping_Topping_ToppingId",
+                        column: x => x.ToppingId,
+                        principalTable: "Topping",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PizzaTopping_ToppingId",
+                table: "PizzaTopping",
+                column: "ToppingId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PizzaTopping");
+
+            migrationBuilder.DropTable(
+                name: "Pizza");
+
+            migrationBuilder.DropTable(
+                name: "Topping");
+        }
+    }
+}
