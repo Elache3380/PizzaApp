b4a26288d26ece27f845df8bc5305a9d8ced9995
Lionel Hardy <l.hardy@afelio.be>
2020-09-04 12:12:02 +0200

    Add project files.

---
+++ PizzaApp.Data/Data/DbInitializer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaApp.Data.Data
+{
+    class DbInitializer
+    {
+    }
+}
---
+++ ServiceLayer/ToppingServices/ToppingService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaApp.ServiceLayer.ToppingServices
+{
+    class ToppingService
+    {
+    }
+}
